eeb73de16766d0c48e9acaaf2e5eed7dd3d4cb2a
Maayan Glikser <maayan@glikm.com>
2015-01-23 19:50:10 +0200

    Add Cat Model

---
+++ Assets/Scripts/NPC/CatAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAnimation : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Awake () {
+		anim.SetBool ("walking", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
