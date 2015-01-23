4e086f26228240e66a525bf2d7cf9fe535cb5cd4
Maayan Glikser <maayan@glikm.com>
2015-01-23 15:09:03 +0200

    Add follow mode to the camera by a public transform

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+	
+	void Start () {
+		offset = transform.position - target.position;
+	}
+
+	void Update () {
+		Vector3 cameraPosition = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, cameraPosition, smoothing * Time.deltaTime);
+	}
+}
