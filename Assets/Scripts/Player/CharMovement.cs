94955442ae8090ad478686ccac5be91467107263
romansky <roman.land@gmail.com>
2015-01-23 15:13:35 +0200

    my initial commit

---
+++ Assets/Scripts/Player/CharMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMovement : MonoBehaviour {
+
+	public GameObject goTerrain;
+
+	private float dist;
+	public float speed = 5f;
+	// last click target location
+	private Vector3 targetLocation;
+
+	void Start () {
+		dist = transform.position.y; // Distance camera is above map
+	}
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (goTerrain.collider.Raycast(ray, out hit, Mathf.Infinity)){
+				targetLocation = hit.point;
+			}
+		}
+
+		if (targetLocation != null) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, targetLocation, step);
+		}
+
+	}
+}
